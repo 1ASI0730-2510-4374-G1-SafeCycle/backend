89479e043b0187bcf7212b544fb086f24d9d53aa
diegoalonzomora <u20231c069@upc.edu.pe>
2025-06-01 19:01:14 -0500

    feat : add swagger for api test

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Adding Swagger as a Service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 
+// Add Swagger for use on Development
 
 if (app.Environment.IsDevelopment())
 {
-  
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
