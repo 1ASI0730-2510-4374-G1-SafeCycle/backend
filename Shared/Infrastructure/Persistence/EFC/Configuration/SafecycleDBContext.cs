70aace0ff4f0ea4338171a6aa932719a0cc4d2bd
diegoalonzomora <u20231c069@upc.edu.pe>
2025-06-02 11:03:12 -0500

    feat : model User table on method OnModelCreating

--- Shared/Infrastructure/Persistence/EFC/Configuration/SafecycleDBContext.cs
+++ Shared/Infrastructure/Persistence/EFC/Configuration/SafecycleDBContext.cs
@@ -1,8 +1,27 @@
+using backend.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using backend.User_Management.Domain.Model.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 public class SafecycleDBContext : DbContext
 {
-    
+    public DbSet<User> Users { get; set; }
+    public SafecycleDBContext(DbContextOptions<SafecycleDBContext> options) : base(options){}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // add builder entities
+        modelBuilder.Entity<User>().HasKey(x => x.Id);
+        modelBuilder.Entity<User>().Property(x => x.Id).ValueGeneratedOnAdd();
+        modelBuilder.Entity<User>().Property(x => x.Username).IsRequired();
+        modelBuilder.Entity<User>().Property(x => x.Email).IsRequired();
+        modelBuilder.Entity<User>().Property(x => x.Password).IsRequired();
+        modelBuilder.Entity<User>().Property(x => x.TypeUser).IsRequired();
+        modelBuilder.Entity<User>().Property(x => x.MaxDailyReservationHour).IsRequired();
+        modelBuilder.Entity<User>().Property(x => x.IdentificationUser).IsRequired();
+        
+        modelBuilder.UseSnakeCaseNamingConvention();
+    }
 }
