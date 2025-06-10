e48c7d24de44f95fdc6f27859d89c34a0de6a216
yigabriel <gabrielsacc7@gmail.com>
2025-06-10 15:28:13 -0500

    fix: wrong namespace

--- Bikes/Infrastructure/Repositories/BikeStationsRepository.cs
+++ Bikes/Infrastructure/Repositories/BikeStationsRepository.cs
@@ -3,7 +3,7 @@ using backend.Bikes.Domain.Repositories;
 using backend.Shared.Infrastructure.Persistence.EFC.Configuration;
 using backend.Shared.Infrastructure.Persistence.EFC.Repositories;
 
-namespace backend.Bike_Management.Infrastructure.Repositories;
+namespace backend.Bikes.Infrastructure.Repositories;
 
 public class BikeStationsRepository(SafecycleDBContext context) : BaseRepository<BikeStations>(context), IBikeStationRepository
 {
