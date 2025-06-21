7d482487332a3aec0a3b6a422f2c6ad956985f62
yigabriel <gabrielsacc7@gmail.com>
2025-06-21 11:12:49 -0500

    feat: add tour booking audit

---
+++ Tours/Domain/Model/Aggregates/TourBooking.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using backend.Renting.Domain.Model.Aggregates;
+using backend.Tours.Domain.Model.Commands;
+using backend.Tours.Domain.Model.Entities;
+namespace backend.Tours.Domain.Model.Aggregates;
+
+public partial class TourBooking
+{
+    protected TourBooking()
+    {
+        tourId = 0;
+        rentId = 0;
+    }
+
+    public TourBooking(CreateTourBookingCommand command)
+    {
+        tourId = command.tourId;
+        rentId = command.rentId;
+    }
+    public int Id { get; set; }
+    
+    [Required]
+    public int tourId { get; set; }
+    [Required]
+    public int rentId { get; set; }
+    
+    [Required]
+    [ForeignKey("tourId")]
+    public required Tour tour {get; set;}
+    
+    [Required]
+    [ForeignKey("rentId")]
+    public required Rent rent{get; set;}
+
+}
