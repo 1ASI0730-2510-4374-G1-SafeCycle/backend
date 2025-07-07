8e67f7d19305de89332b6b5987fd825473cbb620
yigabriel <gabrielsacc7@gmail.com>
2025-07-07 18:00:39 -0500

    feat: connection to database onrender

--- obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2847e90a5176d81543c8bbfa9b6f868146aa9f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f2ff40206b9547097052b7613873054493fc659")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
