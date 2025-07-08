a3419667ceafd4c862cd4dd274552e6288c77ae7
Gonzales Valverde Carlos Matthew <u202314130@upc.edu.pe>
2025-07-08 16:01:21 -0500

    feat: add get tour

--- Tours/Interfaces/REST/ToursController.cs
+++ Tours/Interfaces/REST/ToursController.cs
@@ -50,6 +50,22 @@ public class TourController(IToursCommandService tourCommandService, IToursQuery
         
     }
 
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all Tours",
+        Description = "Returns a list of all Tours",
+        OperationId = "GetAllTours"
+    )]
+    [SwaggerResponse(200, "List of tours returned successfully.")]
+    public async Task<ActionResult> GetAllTours()
+    {
+        var query = new GetAllToursQuery();
+        var result = await tourQueryService.Handle(query);
+        var resources = result.Select(ToursResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(resources);
+    }
+
+
     [HttpPut("{id}")]
     [SwaggerOperation(
         Summary = "Update a Tour",
